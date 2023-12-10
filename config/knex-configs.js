b2002b66a7444f4648a4843026eab244fc5037a3
danielteel <danjteel@gmail.com>
2023-12-10 15:34:40 -0700

    asd

--- config/knex-configs.js
+++ config/knex-configs.js
@@ -7,7 +7,7 @@ module.exports = {
     connection: {
       host: '127.0.0.1',
       port: 5432,
-      database: 'hsecapi',
+      database: 'hsec-api',
       user:     'pi',
       password: 'pipass'
     },
