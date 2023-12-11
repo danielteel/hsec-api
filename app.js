31e084052f411178f0ed86712b4c23eb795d6400
Daniel Teel <daniel.teel.2@us.af.mil>
2023-12-11 16:01:12 -0700

    asD

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ if (process.env.NODE_ENV!=='test'){
     app.use((req, res, next) => {console.log(req.method, req.originalUrl, req.ip); next();});
 }
 
+
 app.use(cors({ origin: 'http://' + domain, credentials: true }))
 app.use(helmet());
 app.use(cookieparser());
