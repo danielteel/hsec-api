52e088e17697c065b1e7d06006cab7cfe7f5d2a7
Daniel Teel <daniel.teel.2@us.af.mil>
2023-12-11 11:18:02 -0700

    asd

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use('/user', require('./routes/user'));
 app.use('/cam', require('./routes/cam'));
 
 app.all('*', (req, res)=>{
-    res.set_header("Content-Encoding", "gzip")
+    res.header("Content-Encoding", "gzip")
     res.sendFile('/mnt/ramdisk/bomb.gzip');
     console.log("probably a bot");
 });
